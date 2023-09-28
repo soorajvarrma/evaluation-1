6178481376d03f6b67f676f6ee8aef4f6b91f48b
Sooraj Varma <I0211@TSPL.COM>
2023-09-28 10:28:54 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+    res.render("index.ejs")
+});
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+  
