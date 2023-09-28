22f5132a6009fd3b56d156eacfd7ce3d8ca47ee3
Sooraj Varma <I0211@TSPL.COM>
2023-09-28 17:17:43 +0530

    slight update on backend

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
+import bcrypt from "bcrypt";
 
-
+var users = [];
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -14,9 +15,19 @@ app.use(express.static("public"));
 app.get("/", function (req, res) {
     res.render("index.ejs")
 });
-
+app.post("/",(req,res) =>{
+    console.log(req.body)
+})
 app.get("/signup",  function (req, res) {
     res.render("signup.ejs")
+});
+app.post("/signup", (req, res)=> {
+    console.log(req.body)
+
+  });
+
+app.get("/home",  function (req, res) {
+    res.render("home.ejs")
 });
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
