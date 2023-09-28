70f4a045bc163f86e90a56307ed2ed26a9a17f23
Sooraj Varma <I0211@TSPL.COM>
2023-09-28 15:50:05 +0530

    Update the front-end

--- index.js
+++ index.js
@@ -9,8 +9,14 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+
+
 app.get("/", function (req, res) {
     res.render("index.ejs")
+});
+
+app.get("/signup",  function (req, res) {
+    res.render("signup.ejs")
 });
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
