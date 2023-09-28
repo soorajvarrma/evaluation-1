f04c8786768d5c17751cadbc4de0787a5b79ee74
Sooraj Varrma <varrmasooraj@gmail.com>
2023-09-29 00:28:08 +0530

    Connect to database

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import express from "express";
 import bodyParser from "body-parser";
-import axios from "axios";
-import bcrypt from "bcrypt";
+import { MongoClient } from "mongodb";
 
-var users = [];
+const uri = "mongodb+srv://soorajvarrma:Uq5IEd6zG4JeZJoD@mycluster.wqzwe4i.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
+
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -15,15 +16,52 @@ app.use(express.static("public"));
 app.get("/", function (req, res) {
     res.render("index.ejs")
 });
-app.post("/",(req,res) =>{
-    console.log(req.body)
+app.post("/", async (req,res) =>{
+    try {
+        await client.connect();
+        const db = client.db('evaluation');
+        const collection = db.collection('users');
+    
+        // Find the document in the collection
+        const first = await collection.findOne({
+            email: req.body.email,
+            password: req.body.password          
+        });
+        if(first){
+            res.render("home.ejs" ,{
+                emailid : req.body.email
+            })
+        }else{
+            res.redirect("/")
+        }
+      }finally {
+        // Close the database connection when finished or an error occurs
+        await client.close();
+      }
+
 })
 app.get("/signup",  function (req, res) {
     res.render("signup.ejs")
 });
-app.post("/signup", (req, res)=> {
-    console.log(req.body)
-
+app.post("/signup", async (req, res)=> {
+    try {
+        await client.connect();
+        const db = client.db('evaluation');
+        const collection = db.collection('users');
+    
+        // Find the first document in the collection
+        const first = await collection.insertOne({
+          email: req.body.email,
+          password: req.body.password,
+          company: req.body.company,
+          gender: req.body.gender
+        });
+        console.log(first);
+      } finally {
+        // Close the database connection when finished or an error occurs
+        await client.close();
+      }
+    res.redirect("/")
   });
 
 app.get("/home",  function (req, res) {
